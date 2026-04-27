f490c0da4092d1a0ede297037fb387384d6145da
Roman <romazaniosdev@gmail.com>
2026-04-27 10:36:57 +0300

    Initial Commit

---
+++ 146MedRemind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  146MedRemind
+//
+//  Created by Roman on 4/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
